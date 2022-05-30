a3cb295cbd69b6c63a3de2b0905f9181f14495d7
emanleand <emanleand@gmail.com>
2022-05-30 00:24:50 -0300

    database config

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 'use strict';
-
 const mongoose = require('mongoose');
+const config = require('config');
 const app = require('./app');
-const port = process.env.PORT  || 3999;
 
-const user = 'ali';
-const psw = 'ali123';
+const port = config.get('port');
+const { name_db, port_db, user_db, psw_db } = config.get('database');
 
 mongoose.Promise = global.Promise;
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://' + user + ':' + psw + '@localhost:27017/forum_api_rest?authSource=admin', { useNewUrlParser: true })
-.then(() => {
+
+mongoose.connect(`mongodb://${user_db}:${psw_db}@localhost:${port_db}/${name_db}`, {
+  useNewUrlParser: true,
+  keepAlive: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
     console.log('connection success');
-    app.listen(port ,()=>{
-        console.log('server ok');
+    app.listen(port, () => {
+      console.log('server ok');
     })
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  })
